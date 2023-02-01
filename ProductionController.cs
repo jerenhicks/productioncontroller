a6c611a48488c4b4b437566c9a0b52fee55b638c
Jeren Hicks <jeren.hicks@aviture.us.com>
2023-02-01 13:57:17 -0600

    Makes a connect to a cup server.

--- ProductionController.cs
+++ ProductionController.cs
@@ -1,4 +1,9 @@
 using Newtonsoft.Json;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
 
 class ProductionController
 {
@@ -7,6 +12,7 @@ class ProductionController
     public void StartUp()
     {
         List<Cup> cupList = ReadCups();
+        StartServer(cupList[0]);
     }
 
     public List<Cup> ReadCups()
@@ -23,6 +29,23 @@ class ProductionController
             throw new Exception("Could not find the json file");
         }
         return cups.ToList();
+    }
+
+    private bool StartServer(Cup cup)
+    {
+        //Person p = new Person("Tyler", "Durden", 30); // create my serializable object 
+
+
+        TcpClient client = new TcpClient(cup.ip, cup.port); // have my connection established with a Tcp Server 
+
+        IFormatter formatter = new BinaryFormatter(); // the formatter that will serialize my object on my stream 
+
+        NetworkStream strm = client.GetStream(); // the stream 
+        //formatter.Serialize(strm, p); // the serialization process 
+
+        strm.Close();
+        client.Close();
+        return false;
     }
 
 }
